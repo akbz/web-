e0657861a06874443c7eb59344294f78e90e14e0
akbz <47210162+akbz@users.noreply.github.com>
2019-10-01 11:51:37 +0500

    Add files via upload

---
+++ li.php
@@ -0,0 +1,77 @@
+<?php
+
+$a = 3;
+echo $b ;
+
+//1.2
+$a=10;
+$b=2;
+echo $a+$b;
+echo $a-$b;
+echo $a*$b;
+echo $a/$b;
+
+
+//1.3
+
+$c=15;
+$d=2;
+$result=$c+$d;
+echo $result;
+
+//1.4
+
+$a=10;
+$b=2;
+$c=5;
+var_dump($a+$b+$c);
+
+
+//1.5
+
+$a=17;
+$b=10;
+$c=$a-$b;
+$d=7;
+echo $result;
+
+
+
+//2.1
+
+$text='Привет, Мир';
+echo $text;
+echo 'Моя первая программа!';
+
+//2.2
+
+$text1='Привет,' ;
+ $text2='Мир!';
+ echo $text1.$text2;
+
+//2.3
+
+$name='Aliya';
+echo "Привет,$name";
+echo $text='Новый текст';
+
+//2.4
+
+$age=20;
+$height=168;
+echo 'Мой возраст:'.$age| 'Мой рост:'.$height;
+
+//2.5
+$text='abcde';
+echo $var[0]; //выведем букву 'a'
+echo $var[2]; //выведем букву 'c'
+echo $var[4]; //выведем букву 'e'
+//3.1
+$arr =['a']['b']['c'];
+
+/**
+ * Created by PhpStorm.
+ * User: Acer
+ * Date: 01.10.2019
+ * Time: 8:46
+ */
